0ef02b4a6d815004389930013142c14556998367
annn88 <annmariyabiju2028@cs.sjcetpalai.ac.in>
2024-10-15 14:58:14 +0530

    Add files via upload

---
+++ python new.py
@@ -0,0 +1,12 @@
+'''
+python program to check whether the given number is
+positive or not
+author: ann mariya biju
+date:15-10-24
+'''
+
+number=int(input("enter a number"))
+if number>0:
+    print("the given number:",number,"is positive")
+else:
+    print("the given number:",number,"is negative")
