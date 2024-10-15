bc0d4c3f0945d8382a480ca61bda87b6a7a5db20
annn88 <annmariyabiju2028@cs.sjcetpalai.ac.in>
2024-10-15 14:59:45 +0530

    Add files via upload

---
+++ python new 2.py
@@ -0,0 +1,6 @@
+number1=int(input("enter number 1:"))
+number2=int(input("enter number 2:"))
+if number1>number2:
+    print("the larger number is number1")
+elif number2>number1:
+    print("the larger number is number2")
